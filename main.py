8e352476fbf29d26267b9912482429b120760f6d
nsr <serheo99@gmail.com>
2020-02-16 22:42:59 +0300

    Перенос на другой комп

--- main.py
+++ main.py
@@ -1,77 +1,104 @@
-##client = Client()
-##client = Client.from_credentials('serheo99@yandex.ru', 'Sn32089461')
-##client.users_likes_tracks()[0].track.download('example.mp3')
 
-#YANDEX
+
+# YANDEX
+
 from yandex_music.client import Client
-from yandex_music import Search, SearchResult
-#search_reqest = client.search("рив гош")
-#print(search_reqest.best.result.title)
-#print(search_reqest.best.result.getDownloadInfo(True))
-#link = search_reqest.best.result.getDownloadInfo(True)[0].getDirectLink()
-##search_reqest.best.result.download('example.mp3')
+from yandex_music import Album, Playlist
+import discord
+from discord.ext import commands
+
+
+login_parameters = ('serheo99@yandex.ru', 'Sn32089461')
+
+
 class YandexDriver(object):
+
     def __init__(self):
         self.client = Client()
-        self.client = Client.from_credentials('serheo99@yandex.ru', 'Sn32089461')
+        login, passwd = login_parameters
+        self.client = Client.from_credentials(login, passwd)
+        self.queueStatus = False
+        self.container: Album | Playlist
+        self.current_number: [int, int] | int
+
     def searchTrack(self, reqest) -> (str, str):
         search_result = self.client.search(reqest, type_="track")
         track = search_result.tracks.results[0]
-        if track == None:
+        if track is None:
             return None
-        title = track.title
-        album = track.albums[0].title
-        artist = track.artists[0].name
-        info_string = "%s -- %s -- %s"%(title, artist, album)
-        link = ""
+        return self.__getTrackInfo(track);
+
+    def initAlbumFromLink(self, url) -> str:
+        self.__resetQueue()
+        self.current_number = [0, 0]
+        #  сделать сплит урла
+        self.container = self.client.albumsWithTracks(url)
+        if self.container is None:
+            return None
+        self.queueStatus = True
+        return self.container.title
+
+    def getCurrentTrack(self) -> (str, str):
+        if isinstance(self.container, Album):
+            vol_num, track_num = self.current_number
+            track = self.container.volumes[vol_num][track_num]
+            return self.__getTrackInfo(track)
+        if isinstance(self.container, Playlist):
+            track = self.container.tracks[self.current_number].track
+            return self.__getTrackInfo(track)
+
+    def getNextTrack(self) -> (str, str):
+        if isinstance(self.container, Album):
+            vol_num, track_num = self.current_number
+            if track_num + 1 == len(self.container.volumes[vol_num]):
+                self.current_number[1] = 0
+                if vol_num + 1 == len(self.container.volumes):
+                    self.__resetQueue()
+                    return
+                self.current_number[0] += 1
+            else:
+                self.current_number[1] += 1
+                track = self.container.volumes[self.current_number[0]][self.current_number[1]]
+                return self.__getTrackInfo(track)
+        if isinstance(self.container, Playlist):
+            if self.current_number + 1 == len(self.container.tracks):
+                self.__resetQueue()
+                return
+            self.current_number + 1
+            track = self.container.tracks[self.current_number].track
+            return self.__getTrackInfo(track)
+
+    def __getTrackInfo(self, track) -> (str, str):
         list_of_DI = track.getDownloadInfo(True)
+        info_string = "%s -- %s -- %s"%(track.title, track.artists[0].name, track.albums[0].title)
+        link = None
         for info in list_of_DI:
             if info.codec == "mp3" and info.bitrate_in_kbps == 192:
                 link = info.getDirectLink()
                 break
-        return (info_string, link)
-#    def getAlbum(self, url) -> [(str, str)]:
-#        search_result = client.albumsWithTracks(
-#        track = search_result.tracks.results[0]
-#        if track == None:
-#            return None
-#        title = track.title
-#        album = track.albums[0].title
-#        artist = track.artists[0].name
-#        info_string = "%s -- %s -- %s"%(title, artist, album)
-#        link = ""
-#        list_of_DI = track.getDownloadInfo(True)
-#        for info in list_of_DI:
-#            if info.codec == "mp3" and info.bitrate_in_kbps == 192:
-#                link = info.getDirectLink()
-#                break
-#        return (info_string, link)
+        return info_string, link
+
+    def __resetQueue(self):
+        self.container = None
+        self.current_number = 0
+        self.queueStatus = False
+
+# DISCORD
 
 
-#DISCORD
 yDriver = YandexDriver()
+# yDriver.getAlbumFromLink("9823194")
 
-#class MusicQueue(object)
-#    def __init__(self):
-#        self.__queue : [(str, str)] = []
-#        self.current : int = 0
-#    def clear(self):
-#        __queue.clear()
-#    def append(track : (str, str)):
-#        __queue.append(track)
-#    def next
-
-#    def
-import discord
-from discord.ext import commands
 TOKEN = 'Njc2NDU5NDQ1Nzc3MDcyMTMx.XkPngQ.tvpRDqztbDBESGgIxXz8q2vdp88'
 bot = commands.Bot(command_prefix='!')
-#@bot.command(pass_context=True)  # разрешаем передавать агрументы
-#async def test(ctx, arg):  # создаем асинхронную фунцию бота
-#    await ctx.send("Go to ...")  # отправляем обратно аргумент
+
+
 class Music(commands.Cog):
+    
     def __init__(self, bot):
         self.bot = bot
+        self.play_ctx = None
+    
     @commands.command(pass_context=True)
     async def play(self, ctx, *, arg):
         await ctx.send("Search: \"%s\"..."%(arg))
@@ -79,20 +106,19 @@ class Music(commands.Cog):
         if track == None:
             await ctx.send("Track not found!")
             return
-        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(track[1]))
-        ctx.voice_client.play(source, after=lambda e: print('Player error: %s' % e) if e else None)
-        await ctx.send('Now playing: ' + track[0])
+        await self.__playTrack(ctx, track)
 
-#    @commands.command(pass_context=True)
-#    async def play_album(self, ctx, *, arg):
-#        await ctx.send("Search: \"%s\"..."%(arg))
-#        track = yDriver.searchTrack(arg)
-#        if track == None:
-#            await ctx.send("Track not found!")
-#            return
-#        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(track[1]))
-#        ctx.voice_client.play(source, after=lambda e: print('Player error: %s' % e) if e else None)
-#        await ctx.send('Now playing: ' + track[0])
+    @commands.command(pass_context=True)
+    async def play_album(self, ctx, *, arg):
+        await ctx.send("Search: \"%s\"..." % (arg))
+        album_name = yDriver.initAlbumFromLink(arg)
+        if (not yDriver.queueStatus):
+            await ctx.send("Album not found!")
+            return
+        await ctx.send("Found: %s" % (album_name))
+        track = yDriver.getCurrentTrack()
+        await self.__playTrack(ctx, track)
+
     @commands.command()
     async def pause(self, ctx):
         """Pause player"""
@@ -102,6 +128,7 @@ class Music(commands.Cog):
                 vc.pause();
                 return
         await ctx.send("Nothing is playing now")
+        
     @commands.command()
     async def resume(self, ctx):
         """Resume player"""
@@ -121,12 +148,16 @@ class Music(commands.Cog):
             return await ctx.send("Not connected to a voice channel.")
         ctx.voice_client.source.volume = volume / 100
         await ctx.send("Changed volume to {}%".format(volume))
+
     @commands.command()
     async def stop(self, ctx):
         """Stops and disconnects the bot from voice"""
         await ctx.voice_client.disconnect()
+
     @play.before_invoke
+    @play_album.before_invoke
     async def ensure_voice(self, ctx):
+        self.play_ctx = ctx
         if ctx.voice_client is None:
             if ctx.author.voice:
                 await ctx.author.voice.channel.connect()
@@ -136,9 +167,21 @@ class Music(commands.Cog):
         elif ctx.voice_client.is_playing():
                 ctx.voice_client.stop()
 
+    async def __playTrack(self, ctx, track):
+        info_string, url = track
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(url))
+        ctx.voice_client.play(source, after=await self.__afterTrack)
+        await ctx.send('Now playing: ' + info_string)
+
+    async def __afterTrack(self, error):
+        if error:
+            self.play_ctx.send("Player error: %s" % error)
+            return
+        if not yDriver.queueStatus:
+            return
+        track = yDriver.getNextTrack()
+        await self.__playTrack(self.play_ctx, track)
+
+
 bot.add_cog(Music(bot))
 bot.run(TOKEN)
-
-
-С уважением,
-Сергей Никифоров
